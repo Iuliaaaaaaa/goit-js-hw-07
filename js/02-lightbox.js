7437bf1ae2b56752cd6db4f8e21338e89586aabb
Iuliaaaaaaa <132394844+Iuliaaaaaaa@users.noreply.github.com>
2023-09-04 21:49:17 +0300

    hw7/2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const placeElementRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+.map(
+    (item) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>`
+)
+.join("");
+
+placeElementRef.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
