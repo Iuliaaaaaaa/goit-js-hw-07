e49b57dc5fe136db38f0882c0d40b79f9d1a5b7a
Iuliaaaaaaa <132394844+Iuliaaaaaaa@users.noreply.github.com>
2023-09-05 14:11:55 +0300

    Change

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,8 +8,8 @@ const galleryMarkup = galleryItems
 .map(
     (item) =>
     `<li class="gallery__item">
-   <a class="gallery__link" href="large-image.jpg">
-      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
    </a>
 </li>`
 )
